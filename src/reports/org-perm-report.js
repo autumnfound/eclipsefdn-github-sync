851a2e1db5c2a1da7002dc564a579479e04ca155
Martin Lowe <martin.lowe@eclipse-foundation.org>
2020-05-21 13:10:50 -0400

    Add throttle config value to org permission report
    Fixed spacing + added config for throttle time.
    
    Signed-off-by: Martin Lowe <martin.lowe@eclipse-foundation.org>

--- src/reports/org-perm-report.js
+++ src/reports/org-perm-report.js
@@ -15,9 +15,28 @@ const Wrapper = require('../GitWrapper.js');
 const CachedHttp = require('../HttpWrapper.js');
 const axios = require('axios');
 const parse = require('parse-link-header');
+// set up yargs command line parsing
+var argv = require('yargs')
+  .usage('Usage: $0 [options]')
+  .example('$0', '')
+  .option('V', {
+    alias: 'verbose',
+    description: 'Sets the script to run in verbose mode',
+    boolean: true
+  })
+  .option('t', {
+    alias: 'throttle',
+    description: 'The number of milliseconds to wait between calls',
+    default: 333
+  })
+  .help('h')
+  .alias('h', 'help')
+  .version('0.1')
+  .alias('v', 'version')
+  .epilog('Copyright 2019 Eclipse Foundation inc.')
+  .argv;
 
 var readline = require('readline');
-
 // create global placeholder for wrapper
 var wrap;
 var cHttp;
@@ -27,7 +46,6 @@ var bots;
 // thread sleeping to prevent abuse of API
 var sab = new SharedArrayBuffer(1024);
 var int32 = new Int32Array(sab);
-const waitTimeInMS = 333;
 // fields desired for report
 var fields = [
   "id",
@@ -75,90 +93,96 @@ async function run(secret) {
   wrap = new Wrapper(secret);
   cHttp = new CachedHttp();
   
-  // get eclipse api data  
+  // get eclipse api data
   var data = retrieveMaintainedOrgRepos(await eclipseAPI());
   var regex = new RegExp(".*(-committers|-contributors|-project-leads)$");
   var rows = [];
   var keys = Object.keys(data);
   for (var idx in keys) {
-	  var org = keys[idx];
-	  var orgActual = await wrap.getOrganization(org);
-	  if (orgActual == undefined) {
-		  console.log(`Error retrieving org '${org}', will skip`);
-		  continue;
-	  }
-	  // get the keys for the data to filter out
-	  var orgFields = Object.keys(orgActual);
-	  // filter out data we don't need
-	  for (var orgIdx in orgFields) {
-	    var field = orgFields[orgIdx];
-	    if (!fields.includes(field)) {
-		  delete orgActual[field];
-	    }
-	  }
-	  // get all repos for org
-	  await wrap.prefetchRepos(org);
-	  await wrap.prefetchTeams(org);
-	  // get copy of all found repos for org
-	  var repos = await wrap.getDiscoveredRepos(org);
-	  for (var rIdx in repos) {
-		  var foundRepo = repos[rIdx];
-		  console.log(`Repo name: ${foundRepo.name}`);
-		  // check if current repo is managed
-		  var teams = await wrap.getTeamsForRepo(org, foundRepo.name);
-		  // if missing teams, error and include
-		  var missingTeam = true;
-		  for (var tIdx in teams) {
-			  var team = teams[tIdx];
-			  if (regex.test(team.name)) {
-				  missingTeam = false;
-				  break;
-			  }
-		  }
-		  
-		  var hasInvalidAdmin = false;
-		  // if member has admin that isn't expected, error and include
-		  var collabs = await wrap.getRepoCollaborators(org, foundRepo.name, "all");
-		  for (var cIdx in collabs) {
-			  var collaborator = collabs[cIdx];
-			  console.log(collaborator)
-			  if (collaborator.permissions.admin == true && !allowedAdmins.includes(collaborator.login)) {
-				  console.log(`Repo ${org}/${foundRepo.name} has invalid admin of ${collaborator.login}`);
-				  hasInvalidAdmin = true;
-			  }
-		  }
-		  console.log(`foundRepo.name: ${foundRepo.name}, hasInvalidAdmin: ${hasInvalidAdmin}, missingTeam: ${missingTeam}`);
-		  if (hasInvalidAdmin || missingTeam) {
-			  // make a copy of current data
-			  var row = JSON.parse(JSON.stringify(orgActual));
-			  // add repo for tracking
-			  row.repo = foundRepo.name;
-			  row.html_url = foundRepo.html_url;
-			  // custom info fields
-			  row.missing_teams = missingTeam;
-			  row.invalid_admin_user = hasInvalidAdmin;
-			  // add to report
-			  rows.push(row);
-		  }
-	  }
+    var org = keys[idx];
+    var orgActual = await wrap.getOrganization(org);
+    wait();
+    if (orgActual == undefined) {
+      console.log(`Error retrieving org '${org}', will skip`);
+      continue;
+    }
+    // get the keys for the data to filter out
+    var orgFields = Object.keys(orgActual);
+    // filter out data we don't need
+    for (var orgIdx in orgFields) {
+      var field = orgFields[orgIdx];
+      if (!fields.includes(field)) {
+      delete orgActual[field];
+      }
+    }
+    // get all repos for org
+    await wrap.prefetchRepos(org);
+    wait();
+    await wrap.prefetchTeams(org);
+    wait();
+    // get copy of all found repos for org
+    var repos = await wrap.getDiscoveredRepos(org);
+    wait();
+    
+    for (var rIdx in repos) {
+      var foundRepo = repos[rIdx];
+      // check if current repo is managed
+      var teams = await wrap.getTeamsForRepo(org, foundRepo.name);
+      wait();
+      // if missing teams, error and include
+      var missingTeam = true;
+      for (var tIdx in teams) {
+        var team = teams[tIdx];
+        if (regex.test(team.name)) {
+          missingTeam = false;
+          console.log(`Found team name '${team.name}' for repo: ${foundRepo.name}, assuming teams are managed through API`);
+          break;
+        }
+      }
+      
+      var hasInvalidAdmin = false;
+      // if member has admin that isn't expected, error and include
+      var collabs = await wrap.getRepoCollaborators(org, foundRepo.name, "all");
+      wait();
+      for (var cIdx in collabs) {
+        var collaborator = collabs[cIdx];
+        if (collaborator.permissions.admin == true && !allowedAdmins.includes(collaborator.login)) {
+          console.log(`Repo ${org}/${foundRepo.name} has invalid admin of ${collaborator.login}`);
+          hasInvalidAdmin = true;
+        }
+      }
+      console.log(`repo.name: ${foundRepo.name}, hasInvalidAdmin: ${hasInvalidAdmin}, missingTeam: ${missingTeam}`);
+      if (hasInvalidAdmin || missingTeam) {
+        // make a copy of current data
+        var row = JSON.parse(JSON.stringify(orgActual));
+        // add repo for tracking
+        row.repo = foundRepo.name;
+        row.html_url = foundRepo.html_url;
+        // custom info fields
+        row.missing_teams = missingTeam;
+        row.invalid_admin_user = hasInvalidAdmin;
+        // add to report
+        rows.push(row);
+      }
+    }
   }
   
-  //print data to console
+  // print data to console
   var header = "";
   for (var fieldIdx in fields) {
-	  header += fields[fieldIdx].toUpperCase() + ",";
+    header += fields[fieldIdx].toUpperCase() + ",";
   }
   console.log(header);
   for (var rowIdx in rows) {
-	  var row = rows[rowIdx];
-	  var out = "";
-	  for (var fieldIdx in fields) {
-		  out += row[fields[fieldIdx]];
-		  if (fieldIdx < (fields.length - 1)) {
-			  out += ",";
-		  }
-	  }
-	  console.log(out);
+    var row = rows[rowIdx];
+    var out = "";
+    for (var fieldIdx in fields) {
+      out += row[fields[fieldIdx]];
+      if (fieldIdx < (fields.length - 1)) {
+        out += ",";
+      }
+    }
+    console.log(out);
   }
   cHttp.close();
 }
@@ -195,32 +219,39 @@ async function eclipseAPI() {
 }
 
 function retrieveMaintainedOrgRepos(data) {
-  	// maintain map of orgs to repos
+    // maintain map of orgs to repos
     var orgs = {};
-  	for (var key in data) {
-  	    var project = data[key];
-  	    var repos = project.github_repos;
-  	    
-  	    // maintain orgs used by this project
-  	    for (var idx in repos) {
-  	      var repoUrl = repos[idx].url;
-  	      // strip the repo url to get the org + repo
-  	      var match = /\/([^\/]+)\/([^\/]+)\/?$/.exec(repoUrl);
-  	      // check to make sure we got a match
-  	      if (match == null) {
-  	        continue;
-  	      }
-  	      
-  	      // get the org from the repo URL
-  	      var org = match[1];
-  	      var repo = match[2];
-  	      if (orgs[org] == undefined) {
-  	    	  orgs[org] = [];
-  	      }
-  	      if (!orgs[org].includes(repo)) {
-  	    	orgs[org].push(repo);
-  	      }
-  	    }
-  	}
+    for (var key in data) {
+        var project = data[key];
+        var repos = project.github_repos;
+        
+        // maintain orgs used by this project
+        for (var idx in repos) {
+          var repoUrl = repos[idx].url;
+          // strip the repo url to get the org + repo
+          var match = /\/([^\/]+)\/([^\/]+)\/?$/.exec(repoUrl);
+          // check to make sure we got a match
+          if (match == null) {
+            continue;
+          }
+          
+          // get the org from the repo URL
+          var org = match[1];
+          var repo = match[2];
+          if (orgs[org] == undefined) {
+            orgs[org] = [];
+          }
+          if (!orgs[org].includes(repo)) {
+          orgs[org].push(repo);
+          }
+        }
+    }
     return orgs;
+}
+
+function wait() {
+  if (argv.V) {
+    console.log(`Waiting for ${argv.t}ms...`);
+  }
+  Atomics.wait(int32, 0, 0, argv.t);
 }
